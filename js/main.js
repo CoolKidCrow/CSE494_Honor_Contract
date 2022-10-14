6a42951466383f0a1866c2c5e1b70e4aadf15e1e
Nathaniel R <32833323+CoolKidCrow@users.noreply.github.com>
2022-10-14 13:34:49 -0700

    selects setup and display csv data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var cpu_data;
-var gpu_data;
-var ram_data;
+let cpu_data;
+let gpu_data;
+let ram_data;
 
 document.addEventListener('DOMContentLoaded', function () {
     
@@ -11,11 +11,52 @@ document.addEventListener('DOMContentLoaded', function () {
             cpu_data = values[0];
             gpu_data = values[1];
             ram_data = values[2];
+
+            cpu_data.map(function(d) {
+                d["Benchmark"] = +d["Benchmark"];
+
+                var option1 = document.createElement("option");
+                option1.value = d["Brand"] + " " + d["Model"];
+                option1.innerHTML = d["Brand"] + " " + d["Model"];
+                var option2 = document.createElement("option");
+                option2.value = d["Brand"] + " " + d["Model"];
+                option2.innerHTML = d["Brand"] + " " + d["Model"];
+                document.getElementById("cpu_1").appendChild(option1);
+                document.getElementById("cpu_2").appendChild(option2);
+                console.log("add opt");
+            });
+            gpu_data.map(function(d) {
+                d["Benchmark"] = +d["Benchmark"];
+                
+                var option1 = document.createElement("option");
+                option1.value = d["Brand"] + " " + d["Model"];
+                option1.innerHTML = d["Brand"] + " " + d["Model"];
+                var option2 = document.createElement("option");
+                option2.value = d["Brand"] + " " + d["Model"];
+                option2.innerHTML = d["Brand"] + " " + d["Model"];
+                document.getElementById("gpu_1").appendChild(option1);
+                document.getElementById("gpu_2").appendChild(option2);
+            });
+            ram_data.map(function(d) {
+                d["Benchmark"] = +d["Benchmark"];
+
+                var option1 = document.createElement("option");
+                option1.value = d["Brand"] + " " + d["Model"];
+                option1.innerHTML = d["Brand"] + " " + d["Model"];
+                var option2 = document.createElement("option");
+                option2.value = d["Brand"] + " " + d["Model"];
+                option2.innerHTML = d["Brand"] + " " + d["Model"];
+                document.getElementById("ram_1").appendChild(option1);
+                document.getElementById("ram_2").appendChild(option2);  
+            });
+
         });
 });
 
 function draw_chart_1() {
-
+        console.log(cpu_data)
+        console.log(gpu_data);
+        console.log(ram_data);
 }
 
 function draw_chart_2() {
