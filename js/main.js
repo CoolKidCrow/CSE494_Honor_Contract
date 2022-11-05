9fdf2329c3ebcc0059d5f953f3231ef6991a8c25
Nathaniel R <32833323+CoolKidCrow@users.noreply.github.com>
2022-11-04 18:58:14 -0700

    exhaustion is temporary, bad code is forever

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ var cpu_data;
 var gpu_data;
 var ram_data;
 
+var padding = 60;
+var width = 480;
+var height = 400;
+var margin = { top: 40, bottom: 40, right: 20, left: 40 };
+var innerWidth = width - margin.left - margin.right;
+var innerHeight = height - margin.top - margin.bottom;
+
 document.addEventListener('DOMContentLoaded', function () {
-    
-   // Loads csv files and stores them
-   Promise.all([d3.csv('data/CPU_UserBenchmarks.csv'),d3.csv('data/GPU_UserBenchmarks.csv'), d3.csv('data/RAM_UserBenchmarks.csv')])
+
+    // Loads csv files and stores them
+    Promise.all([d3.csv('data/CPU_UserBenchmarks.csv'), d3.csv('data/GPU_UserBenchmarks.csv'), d3.csv('data/RAM_UserBenchmarks.csv')])
         .then(function (values) {
             console.log('data loaded');
             cpu_data = values[0];
@@ -13,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
             ram_data = values[2];
 
             //some good ole fashion data wrangling and also sets up the selects
-            cpu_data.map(function(d) {
+            cpu_data.map(function (d) {
                 d["Benchmark"] = +d["Benchmark"];
 
                 var option1 = document.createElement("option");
@@ -25,9 +32,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById("cpu_1").appendChild(option1);
                 document.getElementById("cpu_2").appendChild(option2);
             });
-            gpu_data.map(function(d) {
+            gpu_data.map(function (d) {
                 d["Benchmark"] = +d["Benchmark"];
-                
+
                 var option1 = document.createElement("option");
                 option1.value = d["Model"];
                 option1.innerHTML = d["Brand"] + " " + d["Model"];
@@ -37,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById("gpu_1").appendChild(option1);
                 document.getElementById("gpu_2").appendChild(option2);
             });
-            ram_data.map(function(d) {
+            ram_data.map(function (d) {
                 d["Benchmark"] = +d["Benchmark"];
 
                 var option1 = document.createElement("option");
@@ -47,8 +54,25 @@ document.addEventListener('DOMContentLoaded', function () {
                 option2.value = d["Model"];
                 option2.innerHTML = d["Brand"] + " " + d["Model"];
                 document.getElementById("ram_1").appendChild(option1);
-                document.getElementById("ram_2").appendChild(option2);  
+                document.getElementById("ram_2").appendChild(option2);
             });
+
+
+            const g = d3.select('#bar_svg_1').append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
+            g.append('g')
+                .attr('id', 'xAxis');
+            g.append('g')
+                .attr('id', 'yAxis');
+
+            const g2 = d3.select('#bar_svg_2').append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
+            g2.append('g')
+                .attr('id', 'xAxis');
+            g2.append('g')
+                .attr('id', 'yAxis');
+
+            var div = d3.select("body").append("div")
+                .attr("class", "tooltip")
+                .style("opacity", 0);
 
             draw_chart_1();
             draw_chart_2();
@@ -61,61 +85,65 @@ function draw_chart_1() {
     var select_ram_1 = document.getElementById("ram_1").selectedIndex;
 
     var aggScore = cpu_data[select_cpu_1]["Benchmark"] + gpu_data[select_gpu_1]["Benchmark"] + ram_data[select_ram_1]["Benchmark"];
-    document.getElementById("agg_score_1").textContent = aggScore;
+    document.getElementById("agg_score_1").textContent = +aggScore;
 
 
     //d3 stuff
     var data = [
-        { name: 'CPU Score', score : cpu_data[select_cpu_1]["Benchmark"]},
-        { name: 'GPU Score', score : gpu_data[select_gpu_1]["Benchmark"]},
-        { name: 'RAM Score', score : ram_data[select_ram_1]["Benchmark"]}
+        { name: 'CPU Score', score: cpu_data[select_cpu_1]["Benchmark"], model: cpu_data[select_cpu_1]["Brand"] + " " + cpu_data[select_cpu_1]["Model"], rank: cpu_data[select_cpu_1]["Rank"] },
+        { name: 'GPU Score', score: gpu_data[select_gpu_1]["Benchmark"], model: gpu_data[select_gpu_1]["Brand"] + " " + gpu_data[select_gpu_1]["Model"], rank: gpu_data[select_gpu_1]["Rank"] },
+        { name: 'RAM Score', score: ram_data[select_ram_1]["Benchmark"], model: ram_data[select_ram_1]["Brand"] + " " + ram_data[select_ram_1]["Model"], rank: ram_data[select_ram_1]["Rank"] }
     ];
 
-    var padding = 60;
-    var width = 480;
-    var height = 400;
-    var radius = ((Math.min(width, height) - padding) / 2);
-    var donutWidth = 60;
+    const xScale = d3.scaleBand()
+        .domain(data.map(function (d) { return d.name; }))
+        .range([0, innerWidth])
+        .padding(0.1);
 
-    var color = d3.scaleOrdinal(d3.schemeSet3);
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) { return d.score; })])
+        .range([innerHeight, 0]);
 
-    var svg = d3.select('#pie_svg_1')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+    var svg = d3.select('#bar_svg_1');
 
-    var arc = d3.arc()
-        .innerRadius(radius - donutWidth)
-        .outerRadius(radius);
-          
-    var pie = d3.pie()
-        .value(function(d) { return d.score; })
-        .sort(null);
+    const g = svg.select('g');
 
-    var text = svg.append('text')
-        .attr('text-anchor', 'middle')
-        .text('');
+    var div = d3.select("body").select(".tooltip")
 
-    var path = svg.selectAll('path')
-        .data(pie(data))
-        .enter()
-        .append('path')
-        .attr('d', arc)
+    var color = d3.scaleLinear()
+        .domain([0, 1, 2])
+        .range(['blue', 'green', 'red'])
+
+    var barchart = g.selectAll('rect')
+        .data(data)
+        .join('rect')
+        .attr('x', d => xScale(d.name))
+        .attr('y', d => yScale(d.score))
+        .attr('width', xScale.bandwidth())
+        .attr('height', d => innerHeight - yScale(d.score))
+        .style('fill', (d, i) => color(i))
         .style('stroke', 'black')
         .style('stroke-width', '1')
-        .attr('fill', function(d, i) { 
-          return color(d.data.name);
+        .on("mousemove", function (event, d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html(d.model + "<br/>" + "Score: " + d.score + "<br/>" + "Rank: " + d.rank)
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
         })
-        .on('mouseover', function(d, i) {
-            d3.select(d.toElement).style('stroke-width', '4');
-            text.text(i.data.name + ": " + i.data.score);
-        })
-        .on('mouseout', function(d, i) {
-            d3.select(d.fromElement).style('stroke-width', '1');
-            text.text('');
+        .on("mouseout", function (d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
         });
+
+    const yAxis = d3.axisLeft(yScale);
+    g.select('#yAxis').call(yAxis);
+
+    const xAxis = d3.axisBottom(xScale);
+    g.select('#xAxis').call(xAxis)
+        .attr('transform', `translate(0, ${innerHeight})`);
 }
 
 function draw_chart_2() {
@@ -124,59 +152,63 @@ function draw_chart_2() {
     var select_ram_2 = document.getElementById("ram_2").selectedIndex;
 
     var aggScore = cpu_data[select_cpu_2]["Benchmark"] + gpu_data[select_gpu_2]["Benchmark"] + ram_data[select_ram_2]["Benchmark"];
-    document.getElementById("agg_score_2").textContent = aggScore;
+    document.getElementById("agg_score_2").textContent = +aggScore;
 
 
     //d3 stuff
     var data = [
-        { name: 'CPU Score', score : cpu_data[select_cpu_2]["Benchmark"]},
-        { name: 'GPU Score', score : gpu_data[select_gpu_2]["Benchmark"]},
-        { name: 'RAM Score', score : ram_data[select_ram_2]["Benchmark"]}
+        { name: 'CPU Score', score: cpu_data[select_cpu_2]["Benchmark"], model: cpu_data[select_cpu_2]["Brand"] + " " + cpu_data[select_cpu_2]["Model"], rank: cpu_data[select_cpu_2]["Rank"] },
+        { name: 'GPU Score', score: gpu_data[select_gpu_2]["Benchmark"], model: gpu_data[select_gpu_2]["Brand"] + " " + gpu_data[select_gpu_2]["Model"], rank: gpu_data[select_gpu_2]["Rank"] },
+        { name: 'RAM Score', score: ram_data[select_ram_2]["Benchmark"], model: ram_data[select_ram_2]["Brand"] + " " + ram_data[select_ram_2]["Model"], rank: ram_data[select_ram_2]["Rank"] }
     ];
 
-    var padding = 60;
-    var width = 480;
-    var height = 400;
-    var radius = ((Math.min(width, height) - padding) / 2);
-    var donutWidth = 60;
+    const xScale = d3.scaleBand()
+        .domain(data.map(function (d) { return d.name; }))
+        .range([0, innerWidth])
+        .padding(0.1);
 
-    var color = d3.scaleOrdinal(d3.schemeSet3);
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) { return d.score; })])
+        .range([innerHeight, 0]);
 
-    var svg = d3.select('#pie_svg_2')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+    var svg = d3.select('#bar_svg_2');
 
-    var arc = d3.arc()
-        .innerRadius(radius - donutWidth)
-        .outerRadius(radius);
-          
-    var pie = d3.pie()
-        .value(function(d) { return d.score; })
-        .sort(null);
+    const g = svg.select('g');
 
-    var text = svg.append('text')
-        .attr('text-anchor', 'middle')
-        .text('');
+    var div = d3.select("body").select(".tooltip")
 
-    var path = svg.selectAll('path')
-        .data(pie(data))
-        .enter()
-        .append('path')
-        .attr('d', arc)
+    var color = d3.scaleLinear()
+        .domain([0, 1, 2])
+        .range(['blue', 'green', 'red'])
+
+    var barchart = g.selectAll('rect')
+        .data(data)
+        .join('rect')
+        .attr('x', d => xScale(d.name))
+        .attr('y', d => yScale(d.score))
+        .attr('width', xScale.bandwidth())
+        .attr('height', d => innerHeight - yScale(d.score))
+        .style('fill', (d, i) => color(i))
         .style('stroke', 'black')
         .style('stroke-width', '1')
-        .attr('fill', function(d, i) { 
-          return color(d.data.name);
+        .on("mousemove", function (event, d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html(d.model + "<br/>" + "Score: " + d.score + "<br/>" + "Rank: " + d.rank)
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
         })
-        .on('mouseover', function(d, i) {
-            d3.select(d.toElement).style('stroke-width', '4');
-            text.text(i.data.name + ": " + i.data.score);
-        })
-        .on('mouseout', function(d, i) {
-            d3.select(d.fromElement).style('stroke-width', '1');
-            text.text('');
+        .on("mouseout", function (d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
         });
+
+    const yAxis = d3.axisLeft(yScale);
+    g.select('#yAxis').call(yAxis);
+
+    const xAxis = d3.axisBottom(xScale);
+    g.select('#xAxis').call(xAxis)
+        .attr('transform', `translate(0, ${innerHeight})`);
 }
